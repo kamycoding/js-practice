acff922d011d5223836800825d44f8a55565de3f
kamycoding <kamyar.zamanfar@gmail.com>
2025-06-24 19:54:24 +0200

    Add exercise1: print age, city, and student status

--- week1/day1/exercise1.js
+++ week1/day1/exercise1.js
@@ -1,1 +1,13 @@
- 
+// Add two numbers and print the result using let and console.log
+// let: for defining numbers
+// console.log: for printing the result
+
+
+// Print age, city, and student status
+let myAge = 27;
+let myCity = "Sankt-Augustin";
+let isStudent = true;
+
+console.log(myAge);
+console.log(myCity);
+console.log(isStudent);
