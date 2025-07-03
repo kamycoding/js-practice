53cf7abc5926a50ce76cb0160a80f4c80a1f6119
kamycoding <kamyar.zamanfar@gmail.com>
2025-07-03 23:28:35 +0200

    bnie

---
+++ 09-Type-Conversion-and-Coercion/index.js
@@ -0,0 +1,21 @@
+//type conversion
+
+const inputYear = "1998";
+console.log(Number(inputYear), inputYear);
+console.log(inputYear + 13, Number(inputYear) + 13);
+
+console.log(Number("kamycoding"));
+console.log(typeof NaN);
+
+console.log(String(13));
+
+//type coercion
+
+console.log("I am" + 13 + "years old");
+console.log("13" - "3" - 6);
+console.log("3" * "13");
+
+let n = "3" + 1;
+n = n - 1;
+console.log(n);
+// bis bald
