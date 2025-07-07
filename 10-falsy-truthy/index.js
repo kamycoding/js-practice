9239929f10c2bbeadcf6995e72524861a75fc8d9
kamycoding <kamyar.zamanfar@gmail.com>
2025-07-07 19:50:18 +0200

    add ex2

--- 10-falsy-truthy/index.js
+++ 10-falsy-truthy/index.js
@@ -59,3 +59,29 @@ if (shouldDrive) {
 } else {
   console.log("someone else should drive");
 }
+
+
+// ex 2
+
+const scoreDolphins = (96, 108, 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win ^__^");
+// } else if (scoreKoalas > scoreDolphins){
+//   console.log("Koalas win :))");
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log("No wins :(");
+// }
+
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("DL WINS");
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log("KL WINS");
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+  console.log("Both WIN");
+} else {
+  console.log("NO WIN :(");
+}
