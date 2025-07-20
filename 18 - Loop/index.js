eda3a1ab7d65ff2277ea36743be9dd733be37349
kamycoding <kamyar.zamanfar@gmail.com>
2025-07-20 19:47:52 +0200

    last update Loop

---
+++ 18 - Loop/index.js
@@ -0,0 +1,34 @@
+// Loop
+// For - While - ForEach - Do While - for in - for of
+
+for (let i = 1; i <= 10; i++) {
+  // codes
+  console.log("[For Loop log]");
+}
+
+let userName = "Kamycoding";
+
+for (let i = 0; i < userName.length; i++) {
+  console.log(userName[i]);
+}
+
+// 0 - 100
+// let i = 0 --> i < 100 --> i = i + 1
+
+for (let i = 0; i <= 100; i++) {
+  console.log(i);
+}
+
+// Way 1
+for (let i = 0; i <= 100; i = i + 2) {
+  console.log(i);
+}
+
+// Way 2
+let i;
+for (i = 100; i >= 0; i = i - 2) {
+  console.log(i);
+}
+// console.log(i); // -2
+
+// bis spater :)
