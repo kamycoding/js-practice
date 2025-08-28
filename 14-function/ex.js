fb9da03e806388999c30a6a85b21cb241e565b82
kamycoding <kamyar.zamanfar@gmail.com>
2025-08-28 18:50:34 +0200

    add forEach

--- 14-function/ex.js
+++ 14-function/ex.js
@@ -28,23 +28,43 @@
 // sum(2, 3, showMessage);
 
 // Function Declaration
-function calcAge(birthYear) {
-  return 2037 - birthYear;
-}
-const age = calcAge(1998);
+// function calcAge(birthYear) {
+//   return 2037 - birthYear;
+// }
+// const age = calcAge(1998);
 
 // Function expression
-const calcAge2 = function (birthyear) {
-  return 2037 - birthyear;
-};
-const age2 = calcAge2(1900);
+// const calcAge2 = function (birthyear) {
+//   return 2037 - birthyear;
+// };
+// const age2 = calcAge2(1900);
 
 // Arrow Function
-const yearsUntilRetriment = (birthYear, firstName) => {
-  const ageUser = 2025 - birthYear;
-  const retriment = 65 - ageUser;
-  // return retriment
-  return `${firstName} retries in ${retriment} years`;
-};
+// const yearsUntilRetriment = (birthYear, firstName) => {
+//   const ageUser = 2025 - birthYear;
+//   const retriment = 65 - ageUser;
+// return retriment
+// return `${firstName} retries in ${retriment} years`;
+// };
 
-console.log(yearsUntilRetriment(1998, "kamycoding"));
+// console.log(yearsUntilRetriment(1998, "kamycoding"));
+// console.log(yearsUntilRetriment(1999, "sog"));
+
+// const countLetter = (str, letter) => {
+//   let counter = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === letter) {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// };
+
+// const resault = countLetter("doosetet daram", "t");
+// console.log(resault);
+
+// forEach
+
+const numbers = [2, 4, 5, 65, 6, 7, 8, 56];
+
+numbers.forEach((i) => console.log(i));
