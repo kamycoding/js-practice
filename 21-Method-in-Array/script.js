fe6000e96c1e83fb1ffeebff3a5d028c79737eee
kamycoding <kamyar.zamanfar@gmail.com>
2025-08-31 20:27:48 +0200

    add Ex for forEach

--- 21-Method-in-Array/script.js
+++ 21-Method-in-Array/script.js
@@ -9,6 +9,24 @@
 
 const users = ["kamy", "sog", "shadi", "tania"];
 
-users.forEach(function (user) {
-  console.log("[Users ForEach Function]");
+// users.forEach(function (user) {
+//   console.log("[Users ForEach Function] :", user);
+// });
+
+const scores = [12, 24, 36, 46, 54];
+
+// scores.forEach(function (score) {
+//   console.log(score);
+// });
+
+const students = [
+  { id: 1, name: "kamy", age: 28, email: "kamy@gmail.com" },
+  { id: 2, name: "sog", age: 27, email: "sog@gmail.com" },
+  { id: 3, name: "shadi", age: 5, email: "shadi@gmail.com" },
+];
+
+students.forEach(function (student) {
+  console.log(
+    `Name ==> ${student.name} / age : ${student.age} / email : ${student.email}`
+  );
 });
