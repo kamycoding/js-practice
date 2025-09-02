bf84d2a87b330882a31c7e345a07ac52e666aea6
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-02 11:32:26 +0200

    add EX for SOME

--- 21-Method-in-Array/script.js
+++ 21-Method-in-Array/script.js
@@ -34,12 +34,46 @@
 // });
 
 //* includes
-const students = [
-  ["kamy", "sog", "tania", "shadi"],
-  { id: 1, name: "kamy", age: 28, email: "kamy@gmail.com" },
-  { id: 2, name: "sog", age: 27, email: "sog@gmail.com" },
-  { id: 3, name: "shadi", age: 5, email: "shadi@gmail.com" },
-];
+// const students = [
+//   { id: 1, name: "kamy", age: 28, email: "kamy@gmail.com" },
+//   { id: 2, name: "sog", age: 27, email: "sog@gmail.com" },
+//   { id: 3, name: "shadi", age: 5, email: "shadi@gmail.com" },
+// ];
 
-let isInStudents = students[2].includes("sog");
-console.log(isInStudents);
+// let isInStudents = students[2].includes("sog");
+// console.log(isInStudents);
+
+//* some
+// const isTrue = students.some(function (student) {
+//   return student.name === "kamy";
+// });
+// console.log(isTrue);
+
+//* EX SOME
+
+const products = [
+  { id: 1, title: "laptop", price: 220, qty: 29 },
+  { id: 2, title: "mouse", price: 34, qty: 29 },
+  { id: 3, title: "hard", price: 56, qty: 0 },
+  { id: 4, title: "handsfree", price: 110, qty: 40 },
+  { id: 5, title: "kyboard", price: 69, qty: 40 },
+];
+const basket = [
+  { id: 4, title: "handsfree", price: 110, qty: 40 },
+  { id: 5, title: "kyboard", price: 69, qty: 40 },
+  { id: 2, title: "mouse", price: 34, qty: 29 },
+];
+const userProductTitle = prompt("Enter the product title :");
+
+if (userProductTitle.length > 0) {
+  const isInShop = products.some(function (product) {
+    return product.title === userProductTitle && product.qty > 0;
+  });
+  if (isInShop === true) {
+  } else {
+    alert("No QTY :(");
+  }
+  console.log(isInShop);
+} else {
+  alert("please write product title");
+}
