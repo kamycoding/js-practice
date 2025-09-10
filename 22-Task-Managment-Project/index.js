0821bd205f0a2bac66d4660ebf693887dda49e55
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-10 09:44:28 +0200

    add primitive and Reference

--- 22-Task-Managment-Project/index.js
+++ 22-Task-Managment-Project/index.js
@@ -1,5 +1,12 @@
+//*Numbers, Strings, Boolean (Primitive)
+//* Objects/Arrays (Reference)
+
 let num1 = 12;
 let num2 = num1;
 num1 = 13;
 console.log("Num1 = " + num1);
 console.log("Num2 = " + num2);
+
+let user1 = { id: 1, name: "kamycoding", age: 27 };
+let user2 = user1;
+let user3 = user2;
