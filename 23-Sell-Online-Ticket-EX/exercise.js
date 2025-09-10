d41e165c0320af9480bc63aa8dedc05712db0632
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-10 16:46:45 +0200

    add way 2 for EX

--- 23-Sell-Online-Ticket-EX/exercise.js
+++ 23-Sell-Online-Ticket-EX/exercise.js
@@ -4,12 +4,26 @@ const provincesData = {
   hessen: ["Frankfurt am Main", "Wiesbaden", "Darmstadt"],
 };
 
-const userProvnice = prompt("Enter Your state :");
-const cleanUserProvince = userProvnice.trim().toLowerCase();
-const foundCities = provincesData[cleanUserProvince];
-console.log(foundCities);
-if (foundCities === undefined) {
-  alert("State not found!");
+//* Way 1
+// const userProvnice = prompt("Enter Your state :");
+// const cleanUserProvince = userProvnice.trim().toLowerCase();
+// const foundCities = provincesData[cleanUserProvince];
+// console.log(foundCities);
+// if (foundCities === undefined) {
+//   alert("State not found!");
+// } else {
+//   alert(`List of Cities : ${foundCities}`);
+// }
+
+//* Way 2
+
+const userProvince = prompt("Enter your state:").trim().toLowerCase();
+if (!userProvince) {
+  alert("Please enter a state name!");
+} else if (!provincesData[userProvince]) {
+  alert("State not found! " + Object.keys(provincesData).join(", "));
 } else {
-  alert(`List of Cities : ${foundCities}`);
+  const cities = provincesData[userProvince].join(", ");
+  alert(`List of Cities: ${cities}`);
+  console.log(provincesData[userProvince]);
 }
