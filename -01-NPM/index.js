f13ce106432250e888270cc3ef7e43ee0544555d
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-10 13:42:16 +0200

    add import and export EX

---
+++ -01-NPM/index.js
@@ -0,0 +1,17 @@
+//* import / export
+
+// import {username, number} from "./utils.js"
+
+// import * as Utils from "./utils.js";
+// console.log(Utils.username, Utils.number);
+
+// import { username as firstName, number } from "./utils.js";
+// console.log(firstName, number);
+
+// import { double } from "./utils.js";
+// console.log(double(12));
+
+import { username, sayHello, skills } from "./utils.js";
+
+console.log(username, skills);
+sayHello();
--- -01-NPM/utils.js
+++ -01-NPM/utils.js
@@ -1,7 +1,18 @@
 //* import / export
 
-let username = "kamycoding";
+// let username = "kamycoding";
 
-let number = 13;
+// let number = 13;
 
-export { username, number };
+// export function double(num) {
+//   return num * 2;
+// }
+
+// export { username, number };
+
+export const username = "kamycoding";
+export function sayHello() {
+  let username = prompt("enter your name :");
+  console.log(`Hello ${username}`);
+}
+export const skills = ["HTMLS", "CSS", "JS", "REACT"];
