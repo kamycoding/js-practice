c46af01ef2ec3cd279e3afc6b59ab400d6c76b1a
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-13 15:11:12 +0200

    ex add validation

--- 23-Calc-Exercise/exercise.js
+++ 23-Calc-Exercise/exercise.js
@@ -1,2 +1,13 @@
-let firstNumber = +prompt("Bitte geben Sie die erste Zahl ein:");
-let secondNumber = +prompt("bitte geben Sie die zweite Zahl ein:");
+let firstNumber = prompt("Bitte geben Sie die erste Zahl ein:");
+let secondNumber = prompt("bitte geben Sie die zweite Zahl ein:");
+
+if (firstNumber.trim() === "" || secondNumber.trim() === "") {
+  alert("Bitte geben Sie ein Zahl ein!");
+}
+
+firstNumber = +firstNumber;
+secondNumber = +secondNumber;
+
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+  alert("Nur Zahlen bitte!");
+}
