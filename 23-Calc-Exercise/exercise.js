2fdad6c200b6f74ca49ae25cd62bd695d5433d93
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-13 16:32:37 +0200

    ex

--- 23-Calc-Exercise/exercise.js
+++ 23-Calc-Exercise/exercise.js
@@ -2,12 +2,28 @@ let firstNumber = prompt("Bitte geben Sie die erste Zahl ein:");
 let secondNumber = prompt("bitte geben Sie die zweite Zahl ein:");
 
 if (firstNumber.trim() === "" || secondNumber.trim() === "") {
-  alert("Bitte geben Sie ein Zahl ein!");
+  alert("Bitte geben Sie eine Zahl ein!");
+} else {
+  firstNumber = +firstNumber;
+  secondNumber = +secondNumber;
+
+  if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    alert("Nur Zahlen bitte!");
+  }
 }
+let userNeed = prompt("was brauchst du ? (+ / - / * / %)");
 
-firstNumber = +firstNumber;
-secondNumber = +secondNumber;
-
-if (isNaN(firstNumber) || isNaN(secondNumber)) {
-  alert("Nur Zahlen bitte!");
+if (userNeed.trim() === "") {
+  alert("...");
+} else if (isNaN(userNeed)) {
+  alert("...");
+} else if (
+  userNeed !== "+" &&
+  userNeed !== "-" &&
+  userNeed !== "*" &&
+  userNeed !== "%"
+) {
+  alert("nur ...");
+} else {
+  //codes
 }
