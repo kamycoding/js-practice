67e1e73bc73c614a4cf6cfcf8a4845cdb0c2678d
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-16 20:47:35 +0200

    add new EX

--- 23-Calc-Exercise/exercise.js
+++ 23-Calc-Exercise/exercise.js
@@ -1,11 +1,21 @@
-const characters =
-  "QWERTYUIOPLKJHGFDSAZXCVBNMmnbvcxzasdfghjklpoiuytrewq1234567890";
+//* EX 01
+// const characters =
+//   "QWERTYUIOPLKJHGFDSAZXCVBNMmnbvcxzasdfghjklpoiuytrewq1234567890";
 
-let captcha = "";
-const charactersLength = characters.length;
+// let captcha = "";
+// const charactersLength = characters.length;
 
-for (let i = 0; i < 5; i++) {
-  const randomIndex = Math.floor(Math.random() * charactersLength);
-  captcha += characters[randomIndex];
-}
-console.log(captcha);
+// for (let i = 0; i < 5; i++) {
+//   const randomIndex = Math.floor(Math.random() * charactersLength);
+//   captcha += characters[randomIndex];
+// }
+// alert(`Please Enter the Captcha : ${captcha}`);
+
+//* EX 02
+
+const products = [
+  { id: 1, name: "laptop", price: 100, shortLink: "pL3j" },
+  { id: 1, name: "mouse", price: 80, shortLink: "ms60" },
+  { id: 1, name: "kyboard", price: 60, shortLink: "dFp2" },
+  { id: 1, name: "light", price: 15, shortLink: "3Ed6" },
+];
