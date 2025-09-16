4dd07f40d4d1e42d0660a5e7e3d7517a4d1d8443
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-16 20:34:48 +0200

    add captcha EX

--- 23-Calc-Exercise/exercise.js
+++ 23-Calc-Exercise/exercise.js
@@ -1,29 +1,11 @@
-let firstNumber = prompt("Bitte geben Sie die erste Zahl ein:");
-let secondNumber = prompt("bitte geben Sie die zweite Zahl ein:");
+const characters =
+  "QWERTYUIOPLKJHGFDSAZXCVBNMmnbvcxzasdfghjklpoiuytrewq1234567890";
 
-if (firstNumber.trim() === "" || secondNumber.trim() === "") {
-  alert("Bitte geben Sie eine Zahl ein!");
-} else {
-  firstNumber = +firstNumber;
-  secondNumber = +secondNumber;
+let captcha = "";
+const charactersLength = characters.length;
 
-  if (isNaN(firstNumber) || isNaN(secondNumber)) {
-    alert("Nur Zahlen bitte!");
-  }
+for (let i = 0; i < 5; i++) {
+  const randomIndex = Math.floor(Math.random() * charactersLength);
+  captcha += characters[randomIndex];
 }
-let userNeed = prompt("was brauchst du ? (+ / - / * / %)");
-
-if (userNeed.trim() === "") {
-  alert("...");
-} else if (isNaN(userNeed)) {
-  alert("...");
-} else if (
-  userNeed !== "+" &&
-  userNeed !== "-" &&
-  userNeed !== "*" &&
-  userNeed !== "%"
-) {
-  alert("nur ...");
-} else {
-  //codes
-}
+console.log(captcha);
