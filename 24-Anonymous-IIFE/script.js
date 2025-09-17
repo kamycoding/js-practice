96c689ec9344e62f39f6a3477dd8dafbc0e7e0d1
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-17 20:21:38 +0200

    add timer EX

--- 24-Anonymous-IIFE/script.js
+++ 24-Anonymous-IIFE/script.js
@@ -4,6 +4,45 @@
 //   console.log(number);
 // });
 
-(function sum() {
-  console.log(1 + 12);
-})();
+// (function sum(num1, num2) {
+//   console.log(num1 + num2);
+// })(1, 12);
+
+//* Method setInterval
+
+// setInterval(function () {
+//   console.log("test log");
+// });
+
+let minutes = +prompt("Enter your Minutes :");
+let second = +prompt("Enter your Second");
+
+let timer = setInterval(function () {
+  if (minutes === 0 && second === 0) {
+    clearInterval(timer);
+  }
+  console.log(`Timer : ${minutes}:${second}`);
+  second--;
+}, 1000);
+
+// let minutes = +prompt("Enter your Minutes:", 1);
+// let seconds = +prompt("Enter your Seconds:", 5);
+
+// let timer = setInterval(function () {
+//   if (minutes === 0 && seconds === 0) {
+//     clearInterval(timer);
+//     console.log("✅ Timer Finished!");
+//   }
+
+//   let formattedMinutes = minutes.toString().padStart(2, '0');
+//   let formattedSeconds = seconds.toString().padStart(2, '0');
+
+//   console.log(`Timer: ${formattedMinutes}:${formattedSeconds}`);
+
+//   if (seconds === 0) {
+//     minutes--;
+//     seconds = 59;
+//   } else {
+//     seconds--;
+//   }
+// }, 1000);
