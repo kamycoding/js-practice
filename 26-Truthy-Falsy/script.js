85ff64a84f2220d4ce78e6a17bb9aa86e763c37c
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-17 21:24:57 +0200

    add DOM

--- 26-Truthy-Falsy/script.js
+++ 26-Truthy-Falsy/script.js
@@ -1,1 +1,33 @@
 //* Truthy & Falsey
+
+//! Numbers --> 0 - !0
+
+//! Strings --> "" - !""
+
+//! Object {} --> all object truthy
+
+//! Arrays [] --> all Arrays truthy
+
+const username = prompt("Enter your username : ");
+if (!username.trim()) {
+  //* "" or "TEXT"
+  alert("Enter your username !");
+}
+
+const number = +prompt("please enter the age :");
+
+if (!number) {
+  alert("Error!");
+} else {
+  alert("Done!");
+}
+
+//! HOISTING
+
+console.log(sum(12, 1));
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sum(12, 1));
