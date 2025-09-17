d6b8dee2ea6be4ef16550b1d992f6038299fdb06
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-17 21:52:22 +0200

    DOM

--- 27-DOM/script.js
+++ 27-DOM/script.js
@@ -1,3 +1,13 @@
 const h1Elems = document.getElementsByTagName("h1");
+const titleElem = document.getElementById("title");
+const listElem = document.getElementById("list");
+const liElems = document.getElementsByClassName("list-item");
+const textElem = document.querySelector(".list-item");
+const textElemnts = document.querySelectorAll(".list-item");
 
 console.log(h1Elems);
+console.log(titleElem);
+console.log(listElem);
+console.log(liElems);
+console.log(textElem);
+console.log(textElemnts);
