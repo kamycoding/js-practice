19d92fa58d5257ce5b8beee2dab6856a5d435bf6
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-17 18:10:32 +0200

    add IIFE func

--- 24-Anonymous-IIFE/script.js
+++ 24-Anonymous-IIFE/script.js
@@ -1,5 +1,9 @@
 //* Anonymous & IIFE Function
 
-[1, 2, 3, 4, 5, 6].forEach(function (number) {
-  console.log(number);
-});
+// [1, 2, 3, 4, 5, 6].forEach(function (number) {
+//   console.log(number);
+// });
+
+(function sum() {
+  console.log(1 + 12);
+})();
