073721734bbbdea89ffd0305ed263d06209526bc
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-17 20:58:49 +0200

    add flag

--- 25-SetTimeout/script.js
+++ 25-SetTimeout/script.js
@@ -1,10 +1,38 @@
 //* SetTimeout
 
-setTimeout(
-  function (num1, num2) {
-    console.log(`Timeout Function --> ${num1} and ${num2} `);
-  },
-  3000,
-  12,
-  9
-);
+// setTimeout(
+//   function (num1, num2) {
+//     console.log(`Timeout Function --> ${num1} and ${num2} `);
+//   },
+//   3000,
+//   12,
+//   9
+// );
+
+//! EX
+
+// let hasProfile = false;
+// setTimeout(function () {
+//   if (!hasProfile) {
+//     alert("Complete your Profile please 😒");
+//   }
+// }, 5000);
+
+//* Flag
+
+const username = prompt("Please Enter your username : ");
+const users = ["kamy", "shadi", "sog", "tania"];
+
+let isLogin = false;
+
+users.forEach(function (user) {
+  if (user === username) {
+    isLogin = true;
+  }
+});
+
+if (isLogin) {
+  alert("Done 😊");
+} else {
+  alert("No 😒");
+}
