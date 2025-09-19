3d40b48b6926995ad7badae25bab76fc32816502
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-19 22:16:08 +0200

    add new kurs

--- 30-Event/script.js
+++ 30-Event/script.js
@@ -1,5 +1,19 @@
+//*Way1
+// btn.onclick = logger;
+// function logger() {
+//   console.log("Clicked!");
+// }
+
+//* way 2
+const itemElems = document.querySelectorAll(".list-item");
 const btn = document.querySelector(".click-btn");
-btn.onclick = logger;
+
+btn.addEventListener("click", logger);
+
 function logger() {
   console.log("Clicked!");
+  itemElems.forEach(function (li) {
+    li.style.color = "red";
+    li.style.fontSize = "36px";
+  });
 }
