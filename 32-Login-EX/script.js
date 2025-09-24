a26197ff374cbef11563a2141cdbb18da1442ce0
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-24 15:01:24 +0200

    init js

---
+++ 32-Login-EX/script.js
@@ -0,0 +1,8 @@
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const loginBtn = document.querySelector("#login-btn");
+
+loginBtn.addEventListener("click", function () {
+  console.log(usernameInput.value);
+  console.log(passwordInputS.value);
+});
