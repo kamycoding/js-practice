c031416bc86cc7b4776e9ec225622a31815874e7
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-24 17:00:00 +0200

    add varriable

--- 32-Login-EX/script.js
+++ 32-Login-EX/script.js
@@ -1,6 +1,9 @@
+const form = document.querySelector("#login-form");
 const usernameInput = document.querySelector("#username");
 const passwordInput = document.querySelector("#password");
 const loginBtn = document.querySelector("#login-btn");
+const usernameError = document.querySelector("#username-error");
+const passwordError = document.querySelector("#password-error");
 
 loginBtn.addEventListener("click", function () {
   console.log(usernameInput.value);
