dcc1ec4736495cf7dec579401703af3a4a83e50e
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-25 17:49:16 +0200

    add review

--- 33 - Review Code/script.js
+++ 33 - Review Code/script.js
@@ -1,4 +1,13 @@
 const numbers = [1, 2, 3, 4, 5];
 const newNumbers = [];
 
-numbers.forEach(() => {});
+numbers.forEach((number, index) => {
+  console.log(`number: ${number}`);
+  console.log(`index: ${index}`);
+  console.log("-----------");
+});
+
+numbers.forEach((number) => {
+  newNumbers.push(number * 2);
+});
+console.log(newNumbers);
