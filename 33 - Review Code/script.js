4d5b1e8adfe1b5e70175d3faf827c1ec5b2c6844
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-28 11:08:23 +0200

    add filter review

--- 33 - Review Code/script.js
+++ 33 - Review Code/script.js
@@ -1,25 +1,29 @@
-const numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 5];
 const newNumbers = [];
 
-numbers.forEach((number, index) => {
-  console.log(`number: ${number}`);
-  console.log(`index: ${index}`);
-  console.log("-----------");
-});
+// numbers.forEach((number, index) => {
+//   console.log(`number: ${number}`);
+//   console.log(`index: ${index}`);
+//   console.log("-----------");
+// });
 
-numbers.forEach((number) => {
-  newNumbers.push(number * 2);
-});
-console.log(newNumbers);
+// numbers.forEach((number) => {
+//   newNumbers.push(number * 2);
+// });
+// console.log(newNumbers);
 
-const newNumbers2 = numbers.map((number) => number * 3);
+// const newNumbers2 = numbers.map((number) => number * 3);
 
-console.log(newNumbers2);
+// console.log(newNumbers2);
 
-const result = numbers.find((number) => {
-  console.log(number);
-  const isEqual = number === 3;
-  console.log(isEqual);
-  return isEqual;
-});
-console.log("result:", result);
+// const result = numbers.find((number) => {
+//   console.log(number);
+//   const isEqual = number === 3;
+//   console.log(isEqual);
+//   return isEqual;
+// });
+// console.log("result:", result);
+
+const resault = numbers.filter((number) => number === 5);
+
+console.log(resault);
