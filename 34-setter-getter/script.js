db85c4ed376a7177a68f940d6f5e894f2d14b314
kamycoding <kamyar.zamanfar@gmail.com>
2025-09-29 16:59:36 +0200

    add closure

--- 34-setter-getter/script.js
+++ 34-setter-getter/script.js
@@ -1,13 +1,40 @@
 //* Setter and Getter
 
-const data = {
-  firstName: "sog",
-  lastName: "design",
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  },
-};
-data.firstName = "kamy";
-data.lastName = "coding";
+// const data = {
+//   firstName: "sog",
+//   lastName: "design",
+//   get fullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+//   set fullName(string) {
+//     [this.firstName, this.lastName] = string.split(" ");
+//   },
+// };
+// data.firstName = "kamy";
+// data.lastName = "coding";
 
-console.log(data.fullName);
+// data.fullName = "kamy coding";
+// console.log(data);
+
+//* Closure - Factory Function
+
+//* State Retention
+//* Encapsulation
+// const like = () => {
+//   let likesCounter = 0;
+//   likesCounter++;
+//   return likesCounter;
+// };
+// console.log(like());
+// console.log(like());
+// console.log(like());
+
+let likesCounter = 0;
+const like = () => {
+  likesCounter++;
+  return likesCounter;
+};
+
+console.log(like());
+console.log(like());
+console.log(like());
