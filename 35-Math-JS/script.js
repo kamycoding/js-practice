bcc654a1095d2db1708b063b55efc92f3ce5b2c0
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-02 19:39:05 +0200

    add get in DATE

--- 35-Math-JS/script.js
+++ 35-Math-JS/script.js
@@ -1,3 +1,15 @@
 // Date Object
-const date = new Date();
+const date = new Date("1998-04-05");
 console.log(date);
+console.log(date.getTime());
+
+const date2 = new Date("1998-08-17");
+console.log(date2);
+console.log(date2.getTime());
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getTime());
