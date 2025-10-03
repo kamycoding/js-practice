1f37ecfbee87f241de963d8ba2e754d56f641b34
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-03 21:33:12 +0200

    add try catch

--- 29-Error-Handling/script.js
+++ 29-Error-Handling/script.js
@@ -1,1 +1,15 @@
 //* Error Handling
+const makeString = (arr) => {
+  console.log(arr.join(" "));
+};
+
+// makeString(["Hello", "world!"]);
+try {
+  makeString(13);
+  console.log("Done!");
+} catch (error) {
+  console.log("Error:", error.message);
+} finally {
+  //clean up function
+  console.log("Final");
+}
