0c71977b03ca18e1ddd9b969620832d0641df209
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-03 21:07:10 +0200

    add ex

--- 35-Math-JS/script.js
+++ 35-Math-JS/script.js
@@ -23,4 +23,22 @@ console.log(Math.floor(number)); // 2
 
 console.log(Math.pow(2, 3)); // 2 ** 3
 console.log(Math.sqrt(4)); // 2
-console.log(Math.abs(number));
+console.log(Math.abs(number)); // 2.63
+
+console.log(Math.min(1, 2, 3, -4, -5)); // -5
+console.log(Math.max(1, 2, 3, -4, -5)); // 3
+
+//* EX RANDOM NUMBERS
+// Generate a random integer
+const generateRandomInteger = (min, max) => {
+  const result = min + Math.floor(Math.random() * (max + 1));
+  return result;
+};
+
+console.log(generateRandomInteger(3, 13));
+
+const determineAge = (birthDate) => {
+  const birthDateMS = new Date(birthDate).getTime();
+  console.log(birthDateMS);
+};
+determineAge("1998-04-05");
