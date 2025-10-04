ebf6df08545b0b6dc3f8e598b10dfcd02dfe5839
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-04 15:06:54 +0200

    add ex

--- 37-DOM-again/script.js
+++ 37-DOM-again/script.js
@@ -3,5 +3,12 @@
 // console.log(document);
 
 //* Single Selector
-const element = document.querySelector(".title");
-console.log(element);
+// const element = document.querySelector("h1")
+// const element = document.querySelector(".title");
+// const element = document.querySelector("#main-title");
+// console.log(element);
+
+//*multiple Selector
+// const elements = document.querySelectorAll("p");
+const elements = document.querySelectorAll(".text");
+console.log(elements);
