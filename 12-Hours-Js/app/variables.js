bc2504a1fbfbbfaec2b5c2e003d10a4964d70982
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-14 01:26:30 +0200

    add new chapter

--- 12-Hours-Js/app/variables.js
+++ 12-Hours-Js/app/variables.js
@@ -1,6 +1,6 @@
-document.getElementById("myH1").textContent = "Hello from JavaScript!";
-document.getElementById("myP").textContent =
-  "This paragraph is also changed by JavaScript.";
+// document.getElementById("myH1").textContent = "Hello from JavaScript!";
+// document.getElementById("myP").textContent =
+//   "This paragraph is also changed by JavaScript.";
 
 // variable = a container that stores a value. Behaves as if it were the value it contains.
 // 1. declaration
@@ -22,10 +22,22 @@ var lastName = "Smith";
 
 let online = true; // boolean
 let offline = false; // boolean
-
+let isSrudent = true;
 console.log(typeof firstName, firstName);
 console.log(`your name is ${firstName}`);
 console.log(typeof age);
 console.log(`You are ${age} years old.`);
 console.log(`The price is ${price}`);
 console.log(`Your GPA is ${gpa}`);
+console.log(`Enrolled: ${isSrudent}`);
+
+let fullName = "kamycoding";
+let myAge = 27;
+let student = false;
+
+document.getElementById("p1").textContent = fullName;
+document.getElementById("p2").textContent = myAge;
+document.getElementById("p3").textContent = student;
+document.getElementById(
+  "p4"
+).textContent = `Name is ${fullName} and my age is ${myAge}. Enrolled: ${student}`;
