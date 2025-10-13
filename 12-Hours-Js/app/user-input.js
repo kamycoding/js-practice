c57656fe444c9341d5655aa1c4048417f5ecfdc0
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-14 01:53:42 +0200

    add new chapter

---
+++ 12-Hours-Js/app/user-input.js
@@ -0,0 +1,10 @@
+// * how to accept user input in JavaScript
+
+//* EASY WAY ==> window prompt
+//* Professional way ==> HTML textbox
+
+let userName = window.prompt("whats your username?");
+console.log(userName);
+alert("Hello " + userName + ", welcome to 12 Hours of JavaScript");
+
+
