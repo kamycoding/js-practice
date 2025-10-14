12fcc85637d8b20108d3af2d32c58f2ba1c266a4
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-14 02:01:06 +0200

    add style

--- 12-Hours-Js/app/user-input.js
+++ 12-Hours-Js/app/user-input.js
@@ -3,8 +3,8 @@
 //* EASY WAY ==> window prompt
 //* Professional way ==> HTML textbox
 
-let userName = window.prompt("whats your username?");
-console.log(userName);
-alert("Hello " + userName + ", welcome to 12 Hours of JavaScript");
+// let userName = window.prompt("whats your username?");
+// console.log(userName);
+// alert("Hello " + userName + ", welcome to 12 Hours of JavaScript");
 
 
