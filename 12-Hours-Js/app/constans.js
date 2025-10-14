b372789064e655d19d6db2bc1ec98184aeb86027
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-14 16:33:59 +0200

    feat: add new files

---
+++ 12-Hours-Js/app/constans.js
@@ -0,0 +1,11 @@
+//* const = a variable that cannot be changed!
+
+let pi = 3.14;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+
+circumference = 2 * pi * radius;
+console.log(circumference);
--- 12-Hours-Js/app/type-conversion.js
+++ 12-Hours-Js/app/type-conversion.js
@@ -1,2 +1,19 @@
 //* Type Conversion = change the data type of a value to another data type
 
+let age = window.prompt("How old are you?");
+age = Number(age); // convert string to number
+age += 1;
+
+console.log(age, typeof age);
+
+let x = "pizza!";
+let y = "pizza!";
+let z = "pizza!";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
