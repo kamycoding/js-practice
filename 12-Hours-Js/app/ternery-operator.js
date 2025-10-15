c17963b112cf8929f8c854540aa53e5549af0ad7
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-15 21:57:31 +0200

    add

--- 12-Hours-Js/app/ternery-operator.js
+++ 12-Hours-Js/app/ternery-operator.js
@@ -1,5 +1,10 @@
 //* ternery operator
+//! condition ? codeIFtrue : codeIFfalse
+// let age = +prompt("Enter your age:");
+// let message = age >= 18 ? "you are eligible" : "you are not eligible";
+// alert(message);
 
-let age = +prompt("Enter your age:");
-let message = age >= 18 ? "you are eligible" : "you are not eligible";
+let isStudent = true;
+
+let message = isStudent ? "You are a student" : "You are not a student";
 alert(message);
