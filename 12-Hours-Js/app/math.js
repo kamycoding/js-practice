6a84f1f728ddbf51b2b2f8ca293bdb43aabb1149
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-15 02:14:47 +0200

    add new PJ

---
+++ 12-Hours-Js/app/math.js
@@ -0,0 +1,14 @@
+//* Math js
+
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 3.2;
+let y = 2;
+let z;
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.sqrt(x);
+// z = Math.abs(x);
+console.log(z);
