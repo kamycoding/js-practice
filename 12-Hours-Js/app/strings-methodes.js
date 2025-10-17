5749d51fdef6553d19b1733825e290b8b34d874e
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-17 23:04:21 +0200

    add

---
+++ 12-Hours-Js/app/strings-methodes.js
@@ -0,0 +1,17 @@
+//* Strings Methodes
+let userName = "kamycoding";
+
+console.log(userName.charAt(0));
+console.log(userName.indexOf("k"));
+console.log(userName.length);
+console.log(userName.trim());
+console.log(userName.toLowerCase());
+console.log(userName.toUpperCase());
+console.log(userName.repeat(3));
+console.log(userName.startsWith("k"));
+console.log(userName.endsWith("g"));
+console.log(userName.includes("k"));
+
+const name = "kamycoding";
+const age = 25;
+const country = "USA";
