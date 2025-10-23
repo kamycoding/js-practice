25d214fb3f88d0b04333b94912f260080aa6372f
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-23 12:39:40 +0200

    add

---
+++ 12-Hours-Js/app/game.js
@@ -0,0 +1,6 @@
+const playerChoice = prompt("Choose Rock, Paper, Scissors:").toLowerCase();
+if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+    alert("Invalid choice! Please choose Rock, Paper, or Scissors.");
+} else {
+    
+}
