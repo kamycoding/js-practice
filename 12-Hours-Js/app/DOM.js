2fa555267f28e25343bd8f8f06c163c3360832cc
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-27 14:06:14 +0100

    add new feat

---
+++ 12-Hours-Js/app/DOM.js
@@ -0,0 +1,16 @@
+// DOM
+
+//! Single Selectors
+
+// const element = document.querySelector("h1");
+// console.log(element);
+//! multiple Selectors
+// const elements = document.querySelectorAll("p");
+// console.log(elements);
+
+const element = document.querySelector(".container");
+element.style.color = "lightblue";
+element.style.backgroundColor = "gray";
+element.style.padding = "20px";
+element.style.borderRadius = "8px";
+element.style.textAlign = "center";
