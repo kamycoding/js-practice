f35a4caed3a2335dd5533b6a7159811feec02cd6
kamycoding <kamyar.zamanfar@gmail.com>
2025-10-27 14:56:54 +0100

    add new

--- 12-Hours-Js/app/DOM.js
+++ 12-Hours-Js/app/DOM.js
@@ -9,8 +9,17 @@
 // console.log(elements);
 
 const element = document.querySelector(".container");
-element.style.color = "lightblue";
-element.style.backgroundColor = "gray";
-element.style.padding = "20px";
-element.style.borderRadius = "8px";
-element.style.textAlign = "center";
+// element.style.color = "lightblue";
+// element.style.backgroundColor = "gray";
+// element.style.padding = "20px";
+// element.style.borderRadius = "8px";
+// element.style.textAlign = "center";
+// element.className = "container2";
+// element.className = "container text"
+element.classList.add("text");
+element.classList.toggle("text");
+setInterval(() => {
+  element.classList.toggle("container2");
+}, 2000);
+
+//! inserting to DOM
