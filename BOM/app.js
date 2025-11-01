78da7902302e631371252904102bd7c68efe7b52
kamycoding <kamyar.zamanfar@gmail.com>
2025-11-01 20:07:30 +0100

    add new

--- BOM/app.js
+++ BOM/app.js
@@ -2,3 +2,6 @@
 
 console.log("outer height :", window.outerHeight);
 console.log("outer width :", window.outerWidth);
+
+console.log("inner height :", window.innerHeight);
+console.log("innerWidth :", window.innerWidth);
