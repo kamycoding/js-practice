40b0c74e91d9d4612903f12203960be8516af008
kamycoding <kamyar.zamanfar@gmail.com>
2025-11-02 21:07:52 +0100

    add new

--- 38-local-storage/script.js
+++ 38-local-storage/script.js
@@ -1,4 +1,6 @@
 //* Local storage example
 
 // local storage ==> persistent - 5mg - origin
+const data = { name: "kamycoding", age: 22 };
+const stringData = JSON.stringify(data);
 localStorage.setItem("name", "kamycoding");
