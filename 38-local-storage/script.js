8e778887ca41af8ef28d4592b25906d051bc1ced
kamycoding <kamyar.zamanfar@gmail.com>
2025-11-03 14:46:02 +0100

    add

--- 38-local-storage/script.js
+++ 38-local-storage/script.js
@@ -4,3 +4,10 @@
 const data = { name: "kamycoding", age: 22 };
 const stringData = JSON.stringify(data);
 localStorage.setItem("name", "kamycoding");
+const dataGet = localStorage.getItem("data");
+const parseData = JSON.parse(dataGet);
+console.log(parseData);
+
+// cookies
+document.cookie = "name=kamycoding";
+document.cookie = "age=28";
