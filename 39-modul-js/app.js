be7df4c873dfa9f1576ebb1817501374c390d5ab
kamycoding <kamyar.zamanfar@gmail.com>
2025-11-03 20:14:08 +0100

    add

--- 39-modul-js/app.js
+++ 39-modul-js/app.js
@@ -1,4 +1,7 @@
-import { name, age, introduce } from "./script.js";
+import { name, age } from "./script.js";
+import show from "./script.js";
 
+// console.log(name, age);
+// introduce();
 console.log(name, age);
-introduce();
+show();
--- 39-modul-js/script.js
+++ 39-modul-js/script.js
@@ -1,8 +1,11 @@
 //! Module in JS | Named & Default
 
 const name = "kamycoding";
-export let age = 27;
+let age = 27;
 
 const show = () => console.log("Hello, World!");
 
-export { name, show as introduce };
+// export { name, show as introduce };
+export { name, age };
+
+export default show;
