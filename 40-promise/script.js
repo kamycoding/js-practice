38a36ec90e92a3f4ac4da36e1cfc2837a73ac9b0
kamycoding <kamyar.zamanfar@gmail.com>
2025-11-04 17:19:38 +0100

    add new'

--- 40-promise/script.js
+++ 40-promise/script.js
@@ -3,11 +3,27 @@
 //   console.log("!");
 // }, 1000);
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("1");
-    resolve();
-  }, 1000);
-});
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("1");
+//     resolve();
+//   }, 1000);
+// });
 
-promise.then(() => console.log("2"));
+// promise.then(() => console.log("2"));
+
+const random = new Promise((resolve, reject) => {
+  console.log("Start");
+  setTimeout(() => {
+    //* Operation
+    const randomNumber = Math.random();
+    if (randomNumber < 0.5) {
+      resolve(randomNumber);
+    } else {
+      reject("Error occurred");
+    }
+  });
+}, 2000);
+
+random.then((result) => console.log(result));
+random.catch((error) => console.log(error));
