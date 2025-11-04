45799c8f14823b8568a8c70333f2cfefff519b24
kamycoding <kamyar.zamanfar@gmail.com>
2025-11-04 17:04:37 +0100

    add new

--- 40-promise/script.js
+++ 40-promise/script.js
@@ -1,1 +1,13 @@
 //* Promises in JavaScript
+// setTimeout(() => {
+//   console.log("!");
+// }, 1000);
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("1");
+    resolve();
+  }, 1000);
+});
+
+promise.then(() => console.log("2"));
