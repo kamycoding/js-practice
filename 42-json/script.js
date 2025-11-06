113cf11f3ab29c50118c6a4e137a42425d9923cc
kamycoding <kamyar.zamanfar@gmail.com>
2025-11-06 15:20:52 +0100

    add new

--- 42-json/script.js
+++ 42-json/script.js
@@ -1,7 +1,9 @@
 const data = {
   name: "kamycoding",
   age: 27,
+  skills: ["JS", "React", "Node.js"],
 };
 
 const jsonData = JSON.stringify(data);
 console.log(jsonData);
+console.log(JSON.parse(jsonData));
