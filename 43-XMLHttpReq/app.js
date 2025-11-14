b9211eec07bcbbfb0f90a64c1d2a63aadf0d0007
kamycoding <kamyar.zamanfar@gmail.com>
2025-11-15 00:49:38 +0100

    add new

--- 43-XMLHttpReq/app.js
+++ 43-XMLHttpReq/app.js
@@ -1,4 +1,5 @@
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://jsonplaceholder.typicode.com/comments?postId=1");
 xhr.send();
-const name = "kamycoding"
+const name = "kamycoding";
+console.log(name);
